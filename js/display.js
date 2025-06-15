9d337fac540d3446536286d7ea3e13c40ff9f65f
Sololitz <194461912+Sololitz@users.noreply.github.com>
2025-06-15 18:57:19 +0700

    Add infinite scroll for message

--- js/display.js
+++ js/display.js
@@ -1,6 +1,16 @@
-document.addEventListener('DOMContentLoaded', async () => {
+let offset = 0;
+let isLoading = false;
+let finished = false;
+let cardNumber;
+const wishContainer = document.querySelector('.wish-container');
+
+async function loadMessages() {
+    if (isLoading || finished) return;
+
+    isLoading = true;
+
     try {
-        const res = await fetch('http://127.0.0.1:5000/api/wish');
+        const res = await fetch(`http://127.0.0.1:5000/api/wish?offset=${offset}`);
         if (!res.ok) {
             console.log('Response status:', res.status);
             return;
@@ -9,9 +19,12 @@ document.addEventListener('DOMContentLoaded', async () => {
         const wishes = await res.json();
         if (!wishes) {
             return;
+        }   
+        
+        if (wishes.length === 0) {
+            finished = true;
+            return;
         }
-
-        let cardNumber = 1;
         
         wishes.forEach(wish => {
             const wishComponent = document.createElement('div');
@@ -34,11 +47,45 @@ document.addEventListener('DOMContentLoaded', async () => {
                     </div>
                     <span class="card-count">#${cardNumber}</span>`;
             
-            document.querySelector('.wish-container').appendChild(wishComponent);
-            cardNumber++;
+            wishContainer.appendChild(wishComponent);
+            cardNumber--;
         });
+
+        offset += wishes.length;
     }
     catch(error) {
         console.log(error);
     }
+
+    isLoading = false;
+};
+
+
+window.addEventListener("scroll", () => {
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight - 100) {
+        loadMessages();
+    }
 });
+
+
+document.addEventListener('DOMContentLoaded', async () => {
+    try {
+        const res = await fetch('http://127.0.0.1:5000/api/wish_count');
+        if (!res.ok) {
+            console.log('Response status:', res.status);
+            return;
+        }
+
+        const count = await res.json();
+        console.log(count)
+        if (!count) {
+            return;
+        }      
+
+        cardNumber = count.count;
+        loadMessages();
+    }
+    catch(error) {
+        console.log(error);
+    }  
+})
